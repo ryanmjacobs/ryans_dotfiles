d094e37657303bf0101efd8b766d24d7434e6ed2
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-05-05 20:02:32 -0700

    Updated dwm config and xinitrc for dwm.

--- .bin/dwm/src/dwm-6.0/config.h
+++ .bin/dwm/src/dwm-6.0/config.h
@@ -17,7 +17,9 @@ static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "School", "Other", "Pref.", "Dev.", "MPV", "Organize", "DL" };
+
+/*                            1,        2,       3,       4,      5,      6,     7,          8,    9    */
+static const char *tags[] = { "School", "Other", "Pref.", "Dev.", "Doc.", "MPV", "Organize", "BG", "DL" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
