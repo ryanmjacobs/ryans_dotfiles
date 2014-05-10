a0021e661bb70b70ea25b45be95615f3c06f0dff
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-05-10 00:58:33 -0700

    Updated dwm to put all MPV videos into its own tag.

--- .bin/dwm/src/dwm-6.0/config.h
+++ .bin/dwm/src/dwm-6.0/config.h
@@ -24,6 +24,7 @@ static const char *tags[] = { "School", "Other", "Organize", "Dev.", "Doc.", "MP
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
+	{ "mpv",      NULL,       NULL,       0x20,         True,        -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
 };
 
