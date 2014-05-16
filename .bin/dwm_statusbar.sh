20d26ac333321b736f9b4b23bdcf5b3ab84edebf
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-05-15 22:06:05 -0700

    Moved the dwm status bar code from .xinit to a separate file: dwm_statusbar.sh

---
+++ .bin/dwm_statusbar.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+################################################################################
+# dwm_statusbar.sh
+#
+# Script to generate the statusbar for dwm.
+#
+# Maintained By: Ryan Jacobs <ryan.mjacobs@gmail.com>
+#
+# May 13, 2014 -> Original creation.
+################################################################################
+
+# Global Constants
+WIFI_DEVICE=wlp2s0
+REFRESH_DELAY=1
+
+while true; do
+    # System Uptime
+    DWM_UPTIME=$(simple_uptime) # Custom C binary to give correct uptime by Ryan
+
+    # Power/Battery Status
+    if [ "$(cat /sys/class/power_supply/AC0/online)" == 1 ]; then
+        DWM_BATTERY="AC"
+        DWM_RENEW_INT=3
+    else
+        DWM_BATTERY=$((`cat /sys/class/power_supply/BAT0/energy_now` * 100 /\
+                       `cat /sys/class/power_supply/BAT0/energy_full`))
+        DWM_RENEW_INT=30
+    fi
+
+    # Wi-Fi eSSID
+    cat /proc/net/route | grep $WIFI_DEVICE &>/dev/null
+    IS_DEVICE_UP=$?
+    if [ "$IS_DEVICE_UP" -eq "0" ]; then
+        DWM_WIFI_ESSID=$(/sbin/iwgetid -r)
+        DWM_WIFI_STRENGTH=$(awk 'NR==3 {print $3"%"}''' /proc/net/wireless |\
+                            tr -d '.')
+    else
+        DWM_WIFI_ESSID="OFF"
+    fi
+
+    # Volume Level
+    DWM_VOL=$(amixer -c0 sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}')
+
+    # Date and Time
+    DWM_CLOCK=$(date '+%a %b %d, %Y | %r')
+
+    # Overall output command
+    DWM_STATUS="\
+Uptime: [$DWM_UPTIME] | WiFi: $DWM_WIFI_ESSID [$DWM_WIFI_STRENGTH] | \
+Power: [$DWM_BATTERY%] | Vol: $DWM_VOL -- $DWM_CLOCK"
+
+    xsetroot -name "$DWM_STATUS"
+    sleep $REFRESH_DELAY
+done
