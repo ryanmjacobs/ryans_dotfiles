8387b93e6e3c35cdf7749f7052626027bfb5b4b1
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-05-30 02:12:53 -0700

    Finally got it right. Now dwm uses the correct tag.

--- .bin/dwm_source/ryans_dwm_config.h
+++ .bin/dwm_source/ryans_dwm_config.h
@@ -32,7 +32,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
-	{ "mpv",      NULL,       NULL,       1 << 3,       True,        -1 },
+	{ "mpv",      NULL,       NULL,       1 << 5,       True,        -1 },
 };
 
 /* layout(s) */
