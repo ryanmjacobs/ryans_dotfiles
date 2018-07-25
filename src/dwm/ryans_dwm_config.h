a0665173ef849ab945e0105dd84c42980e1a27f0
Ryan Jacobs <ryan@rmj.us>
2018-07-25 01:02:27 -0500

    click to kill

--- src/dwm/ryans_dwm_config.h
+++ src/dwm/ryans_dwm_config.h
@@ -131,6 +131,8 @@ static Key keys[] = {
 /* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
     /* click                event mask      button          function        argument */
+    { ClkClientWin, ControlMask|ShiftMask,  Button1,        killclient,     {0} },
+
     { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
     { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
     { ClkWinTitle,          0,              Button2,        zoom,           {0} },
