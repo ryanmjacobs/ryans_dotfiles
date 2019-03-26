45fc48fb2cc12b2f5f4d4ce04b7983c6ebaa0ec0
Ryan Jacobs <ryan@rmj.us>
2019-03-26 06:39:28 -0700

    kinda works

--- bash-completions/play.sh-completion.bash
+++ bash-completions/play.sh-completion.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 _play_sh_completion() {
-    lookup="$HOME/.ytc/lookup.txt"
-    [ ! -e "$lookup" ] && return
-    ./play.sh-completion-helper.c "${COMP_WORDS[@]}"
+    [ ! -e "$HOME/.ytc/lookup.txt" ] && return
+
+    while read valid_term; do
+        COMPREPLY+=("$valid_term")
+    done < <("$DIR/play.sh-completion-helper.c" "${COMP_WORDS[@]}")
 }
 
 complete -F _play_sh_completion play.sh
