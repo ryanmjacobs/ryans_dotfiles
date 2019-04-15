281227abc480a4cbba497a70ba22816aeddc3289
Ryan Jacobs <ryan@rmj.us>
2019-04-15 06:31:54 +0000

    aur: verbose rm

--- aur.sh
+++ aur.sh
@@ -2,7 +2,7 @@
 set -e
 
 tmp="$(mktemp -d)"
-trap "rm -rf $tmp" EXIT INT TERM
+trap "rm -rvf $tmp" EXIT INT TERM
 
 git clone --depth=1 https://aur.archlinux.org/yay.git "$tmp" && cd "$tmp"
 makepkg -si
