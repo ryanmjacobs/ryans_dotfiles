9a988afa8e0acecfcfcaf820d1a77a034fd13abd
Ryan Jacobs <ryan@rmj.us>
2019-04-15 06:34:38 +0000

    remove verbose actually

--- aur.sh
+++ aur.sh
@@ -2,7 +2,7 @@
 set -e
 
 tmp="$(mktemp -d)"
-trap "rm -rvf $tmp" EXIT INT TERM
+trap "rm -rf $tmp" EXIT INT TERM
 
 git clone --depth=1 https://aur.archlinux.org/yay.git "$tmp" && cd "$tmp"
 makepkg -si --noconfirm
