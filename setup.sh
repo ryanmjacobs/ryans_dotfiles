b4c7be2d610bb881ebe7b9281004124dbd6175fd
Ryan Jacobs <ryan@rmj.us>
2019-04-25 12:44:04 -0700

    untested: nvim symlink

--- setup.sh
+++ setup.sh
@@ -118,6 +118,10 @@ for f in ${array[@]}; do
     mkdir -p "$(dirname "$f")"
 done
 
+# nvim config (symlink to .vim)
+mkdir -p ~/.config
+ln -s ~/.vim ~/.config/nvim
+
 # Update Vundle packages
 if type vim &>/dev/null; then
     vim -c BundleInstall -c qa
