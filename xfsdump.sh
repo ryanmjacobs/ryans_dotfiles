042c614847a882ea31f5316d4109bddb83279461
Ryan Jacobs <ryan@rmj.us>
2019-12-14 03:38:45 -0800

    add xfsdump

---
+++ xfsdump.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$HOSTNAME" != "mu2"]; then
+    >&2 echo "error: host not supported"
+    exit 1
+fi
+
+label="$(hostname).l1.$(date +%Y%m%d).xfsdump"
+media="ryans thinkpad x250 internal 500 gb ssd samsung evo"
+time xfsdump -p10 -l1 -L "$label" -f "$label" -M "$media" /dev/sda1
