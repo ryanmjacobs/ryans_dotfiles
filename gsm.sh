db3e97055ac9896aa2fa3817c0450603e8924bf1
Ryan Jacobs <ryan@rmj.us>
2019-12-20 15:33:13 -0500

    retry

--- gsm.sh
+++ gsm.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -x
 
 conn="GSM connection"
-nmcli connection up "$conn"
-nmcli connection modify "$conn" ipv4.dns 1.1.1.1
-nmcli connection modify "$conn" ipv6.dns 2606:4700:4700::1111
+retry_sec=10
+
+sync() {
+    nmcli connection up "$conn"
+    nmcli connection modify "$conn" ipv4.dns 1.1.1.1
+    nmcli connection modify "$conn" ipv6.dns 2606:4700:4700::1111
+}
+
+for n in `seq 3`; do
+    sync && exit 0
+
+    >&2 echo "error: failed to enact settings... retrying in $retry_sec seconds"
+    sleep "$retry_sec"
+done
