24924be3f0a9d1c1df72c3dbcb1760f69c02f349
Ryan Jacobs <ryan@rmj.us>
2020-01-10 02:44:33 -0800

    replace live code reloading with exec

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-while true; do
-    c "$HOME/.bin/rbin/src/dwmstatus.c -lX11 `pkg-config --cflags --libs libnotify`"
-    sleep 0.5
-done
+exec "$HOME"/.bin/rbin/dwmstatus
+
+#while true; do
+#    c "$HOME/.bin/rbin/src/dwmstatus.c -lX11 `pkg-config --cflags --libs libnotify`"
+#    sleep 0.5
+#done
