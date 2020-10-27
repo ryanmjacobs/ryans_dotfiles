22437ba4513eb0d2defc041c40324c967ecf1d2b
Ryan Jacobs <ryan@rmj.us>
2020-10-26 18:58:02 -0700

    group

--- rbin/iommu.sh
+++ rbin/iommu.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-for d in /sys/kernel/iommu_groups/*/devices/*; do     n=${d#*/iommu_groups/*}; n=${n%%/*};     printf 'IOMMU Group %s ' "$n";     lspci -nns "${d##*/}"; done
+
+prev_group=
+
+for d in /sys/kernel/iommu_groups/*/devices/*; do
+    n=${d#*/iommu_groups/*}
+    group=${n%%/*}
+    name="$(lspci -nns "${d##*/}")"
+
+    if [ "$group" != "$prev_group" ]; then
+        echo "----"
+        prev_group="$group"
+    fi
+    echo "IOMMU GROUP $group // $name"
+done
