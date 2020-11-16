c91f67dff521a0e69de69cb9c83f7606d0b9edb0
Ryan Jacobs <ryan@rmj.us>
2020-11-16 08:53:58 -0800

    dire

--- rbin/src/dwm/ryans_dwm_config.h
+++ rbin/src/dwm/ryans_dwm_config.h
@@ -82,9 +82,9 @@ static const char *maim[]            = { "maim", "-s", "screenshot.png", NULL };
 
 /* lock, sleep, hibernate, (or some combination thereof) */
 static const char *lockcmd[]      = { "sh", "-c", "slock", NULL };
-static const char *sleepcmd[]     = { "sh", "-c", "slock & { sleep 1; sudo zzz; }",    NULL };
-static const char *hibernatecmd[] = { "sh", "-c", "slock & { sleep 1; sudo ZZZ; }",    NULL };
-static const char *hybridcmd[]    = { "sh", "-c", "slock & { sleep 1; sudo ZZZ -H; }", NULL };
+static const char *sleepcmd[]     = { "sh", "-c", "slock & { sleep 1; sudo /usr/bin/zzz; }",    NULL };
+static const char *hibernatecmd[] = { "sh", "-c", "slock & { sleep 1; sudo /usr/bin/ZZZ; }",    NULL };
+static const char *hybridcmd[]    = { "sh", "-c", "slock & { sleep 1; sudo /usr/bin/ZZZ -H; }", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
