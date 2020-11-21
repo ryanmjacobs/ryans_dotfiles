338732907b83678214655fec101c662f3b03bdd5
Ryan Jacobs <ryan@rmj.us>
2020-11-20 20:02:43 -0800

    std

--- rbin/src/we.c
+++ rbin/src/we.c
@@ -10,15 +10,24 @@
  *   Date: 24 April 2015
  */
 
+#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
 #include <stdlib.h>
 
 char *basename(char *path) {
     char *base = strrchr(path, '/');
     return base ? base+1 : path;
+}
+
+// return a buffer with a datetime string
+// make sure to free() it after printing
+char *datetime(void) {
+    char *buf = malloc(128);
+    time_t now = time (0);
+    strftime (buf, 100, "%Y-%m-%d %H:%M:%S.000", localtime (&now));
+    return buf;
 }
 
 int main(int argc, char **argv) {
@@ -32,7 +41,12 @@ int main(int argc, char **argv) {
         unsigned int i;
 
         for (i = 1; i < argc; i++) {
-            if (!(access(argv[i], F_OK) != -1)) break;
+            if (!(access(argv[i], F_OK) != -1)) {
+                char *dt = datetime();
+                fprintf(stderr, "%s: file does not exist: %s\n", dt, argv[1]);
+                free(dt);
+                break;
+            }
             if (i == argc-1) return 0;
         }
 
